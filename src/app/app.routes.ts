20d9a5412f7db3a3119c523af69aba5c40e44ad2
Marcel Sörensen <marcel.soerensen@icloud.com>
2026-03-31 21:20:53 +0200

    initial project setup with global styles ui component styles fonts and base structure

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { MainPage } from './main-page/main-page';
+import { CreateSurvey } from './create-survey/create-survey';
+import { Ballot } from './ballot/ballot';
+import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { Imprint } from './imprint/imprint';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {path: '', redirectTo: 'main-page', pathMatch: 'full'},
+    {path: 'main-page', component: MainPage},
+    {path: 'create-survey', component: CreateSurvey},
+    {path: 'ballot', component: Ballot},
+    {path: 'privacy-policy', component: PrivacyPolicy},
+    {path: 'imprint', component: Imprint},
+];
+
+
---
+++ src/app/ballot/ballot.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ballot',
+  imports: [],
+  templateUrl: './ballot.html',
+  styleUrl: './ballot.scss',
+})
+export class Ballot {}
