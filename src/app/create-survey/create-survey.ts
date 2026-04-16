c23cdf62eb11a02fd1ab143edf2e757f7644d55d
Marcel Sörensen <marcel.soerensen@icloud.com>
2026-04-16 11:13:29 +0200

    Implement create survey page layout and styles; add navigation and button functionality

--- src/app/create-survey/create-survey.ts
+++ src/app/create-survey/create-survey.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-create-survey',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './create-survey.html',
   styleUrl: './create-survey.scss',
 })
